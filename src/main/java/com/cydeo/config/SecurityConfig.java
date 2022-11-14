35561c2c2978e93841e08f333b10113db102c9eb
mizgin <mizgincvk@gmail.com>
2022-11-14 16:20:41 -0500

    built flow for security

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -2,12 +2,14 @@ package com.cydeo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,21 +18,44 @@ import java.util.List;
 @Configuration
 public class SecurityConfig {
 
-    // UserDetailsService has loadUserByUsername method -> this method returns UserDetails, User class implements UserDetails
-    // Security only understands User class not ours
-    // Encode means it is converting that password into something no one can understand or read. Saving it in an encrypted way.
+//    @Bean
+//    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+//
+//        List<UserDetails> userList = new ArrayList<>();
+//
+//        userList.add(new User("mike",encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"))));
+//
+//        userList.add(new User("ozzy",encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER"))));
+//
+//
+//        return new InMemoryUserDetailsManager(userList);
+//
+//    }
+
+    //authorizeRequests() -> checking authorization for each
+    //permitAll() -> anybody can access it
+    //httpBasic() -> brings pop up box to enter username and password
     @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-
-        List<UserDetails> userList = new ArrayList<>();
-
-        userList.add(new User("mike",encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"))));
-
-        userList.add(new User("ozzy",encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER"))));
-
-
-        return new InMemoryUserDetailsManager(userList);
-
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeRequests()
+//                .antMatchers("/user/**").hasRole("ADMIN")
+                .antMatchers("/user/**").hasAuthority("ROLE_ADMIN")
+//                .antMatchers("/project/**").hasRole("MANAGER")
+//                .antMatchers("/task/employee/**").hasRole("EMPLOYEE")
+//                .antMatchers("/task/**").hasRole("MANAGER")
+//                .antMatchers("/task/**").hasAnyRole("EMPLOYEE","ADMIN")
+//                .antMatchers("/task/**").hasAuthority("ROLE_EMPLOYEE")
+                .antMatchers("/","/login","/fragments/**","/assets/**","/images/**")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and()
+//                .httpBasic()
+                .formLogin()
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/welcome")
+                    .failureUrl("/login?error=true")
+                    .permitAll()
+                .and().build();
     }
 
 }
